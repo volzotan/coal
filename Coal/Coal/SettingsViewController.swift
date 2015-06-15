f2ea3efe3305ef19c202b4be77365161e52883d3
Christopher Getschmann <christopher@volzo.de>
2015-06-15 22:29:20 +0200

    SettingsView: TextField uncofus on return

--- Coal/Coal/SettingsViewController.swift
+++ Coal/Coal/SettingsViewController.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 
-class SettingsViewController: UITableViewController {
-   
+class SettingsViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("foo")
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
